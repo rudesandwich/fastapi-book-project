c280ccd7fabbe66fc4da871ab59f21dd4c725d3e
Joseph Aniogbe <joaniogbe@gmail.com>
2025-02-14 01:48:23 +0100

    Update books.py

--- api/routes/books.py
+++ api/routes/books.py
@@ -47,7 +47,7 @@ async def get_books() -> OrderedDict[int, Book]:
 
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def get_book_by_id(book_id: int) -> Book:
-    book = db.get_book_by_id(book_id)
+    book = db.get_book(book_id)
     if book is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
